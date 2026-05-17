5f5ea1bab3701d7d501aa95426d071fe0906c402
Roisazzshid3 <isamiq3@gmail.com>
2026-05-17 18:38:08 +0700

    feat: add initial setup for React application with Vite
    - Added React and Vite configuration files.
    - Created main entry point for the application.
    - Included Bootstrap for styling.
    - Added global CSS variables for light and dark themes.
    - Introduced SVG assets for React and Vite logos.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div className="container mt-5 text-center">
+      <h1 className="text-primary">
+        AmbaCart Project Starting...
+      </h1>
+    </div>
+  );
+}
+
+export default App;
