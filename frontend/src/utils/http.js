efc4a71b93c24a34afbafe0a12745b5f6a8fb85e
Roisazzshid <isamiq3@gmail.com>
2026-07-01 11:30:56 +0700

    Update baseURL to use HTTPS for API requests

--- frontend/src/utils/http.js
+++ frontend/src/utils/http.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const http = axios.create({
   // Tetap gunakan IP ini agar terhindar dari bentrok IPv6 (::1)
-  baseURL: 'projectambastackdev-production.up.railway.app/api', 
+  baseURL: 'https://projectambastackdev-production.up.railway.app/api', 
 });
 
 // INTERCEPTOR: Menyisipkan token ke setiap request
